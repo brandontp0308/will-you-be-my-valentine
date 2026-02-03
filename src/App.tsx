bec3132571ea2de35c68ce8d3373abfc855dd1b1
brandontp0308 <brandontp0308@gmail.com>
2026-02-03 19:46:22 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -37,16 +37,16 @@ export default function Page() {
     <div className="-mt-16 flex h-screen flex-col items-center justify-center">
       {yesPressed ? (
         <>
-          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
+          <img src="https://media.tenor.com/wkzCX9s5kxQAAAAi/0906-peacegoma.gif" />
           <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you pookie!! ;))</div>
         </>
       ) : (
         <>
           <img
             className="h-[200px]"
-            src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
+            src="https://media.tenor.com/BL6TlnbQD0sAAAAi/dudu-surprise.gif"
           />
-          <h1 className="my-4 text-4xl">Will you be my Valentine?</h1>
+          <h1 className="my-4 text-4xl">Will you be my Valentine ma bebeee hehe?</h1>
           <div className="flex items-center">
             <button
               className={`mr-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700`}
